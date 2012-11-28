c43888ab082a2fd934a181832a2b6c43c1dc2401
Andres Tarantini <atarantini@gmail.com>
2012-11-27 23:14:42 -0300

    Add debug library and a few debugging lines

--- twittvault.js
+++ twittvault.js
@@ -6,6 +6,7 @@
  */
 
 var config = require('./config.json')
+  , debug = require('debug')('twittvault')
   , api = require('tuiter')(config.twitter_api_keys)
   , mysql = require('mysql');
 
@@ -16,27 +17,31 @@ function save_tweet(data) {
 	  'INSERT INTO '+ config.db.tweets_table + ' SET id_tweet = ?, text = ?, id_user = ?',
 	  [data.id_str, data.text,  data.user.id]
 	);
+	debug('%s stored in db', data.id_str)
 }
 
 
 // DB setup
+debug('Connecting to MySQL')
 var db = mysql.createClient(config.db);
+debug('Using %s database', config.db.name)
 db.query('USE ' + config.db.name);
 
 
 // Get user id from screen name
+debug('Trying to get twitter user id from screen name @%s', config.screen_name)
 api.showUser({screen_name: config.screen_name}, function(err, user) {
 	if(!err) {
 		// Create stream connection to Twitter API
 		api.filter({follow: user.id}, function(stream){
 			stream.on('tweet', function(data){
-				console.log('+ @'+data.user.screen_name+': '+data.text);
+				debug('@%s: %s', data.user.screen_name, data.text);
 				// Store tweet in DB
 				save_tweet(data);
 			});
 		});
 
-		console.log('Waiting for tweets...');
+		debug('Waiting for tweets');
 	} else {
 		console.log('Error when trying to get username id of @'+config.screen_name);
 		process.exit(1);
